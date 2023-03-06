50a30f68f248c1bf814ec08c262b82f8f61a899e
Hugossj3 <gdhugo666@gmail.com>
2023-03-06 23:22:37 +0100

    Segundo Mensaje

--- easyScript.java
+++ easyScript.java
@@ -1,5 +1,6 @@
 public class easyScript{
   public static void main(String[] args){
      System.out.println("Hola Mundo");
+     System.out.println("Scipt prueba para el bisect y el blame de git");
   }
 }
