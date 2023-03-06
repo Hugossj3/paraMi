76a421f3083ce1b306e57495bd10820c2b6c958c
Hugossj3 <gdhugo666@gmail.com>
2023-03-06 23:35:04 +0100

    Acertijo

--- easyScript.java
+++ easyScript.java
@@ -1,6 +1,10 @@
 public class easyScript{
   public static void main(String[] args){
-     System.out.println("Hola Mundo");
-     System.out.println("Scipt prueba para el bisect y el blame de git");
+    int miNum=8; 
+    System.out.println("Hola Mundo");
+    System.out.println("Scipt prueba para el bisect y el blame de git");
+    System.out.println("Adivina mi numero");
+    int adivina=new Scanner(System.in).nextInt();
+    
   }
 }
